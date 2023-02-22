4e41095c8c12a10323188fb553ff67f031aef400
adamFentonAbertay <2001546@uad.ac.uk>
2023-02-22 17:45:33 +0000

    broken ahh narrator movement

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.CompilerServices;
+using UnityEngine.XR.OpenXR.Input;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,11 +13,15 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI dialogueText;
     [SerializeField] int lettersPerSecond = 10;
     [SerializeField] string[] dialoguesScenarios;
+    public RawImage Narrator;
     private int textCountTracker = 0;
+    private bool NarratorMovementFlipFlop = true;
     
     public void showDialogue()
     {
         StopAllCoroutines();
+        Narrator.transform.position.Set(300, 100, 200);
+       
         Debug.Log("show dialgoue");
         dialogueBox.SetActive(true);
         StartCoroutine(TypeDialogue(dialoguesScenarios[textCountTracker]));
@@ -27,6 +33,24 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = "";
         foreach (var letter in dialogue.ToCharArray())
         {
+            if (NarratorMovementFlipFlop == true)
+            {
+                Debug.Log("flip");
+                float posX = Narrator.transform.position.x;
+                float posZ = Narrator.transform.position.z;
+                Narrator.transform.position.Set(posX, 100, posZ);
+                NarratorMovementFlipFlop = false;
+                
+            }
+            else
+            {
+                Debug.Log("flop");
+                float posX = Narrator.transform.position.x;
+                float posZ = Narrator.transform.position.z;
+                Narrator.transform.position.Set(posX, -100, posZ);
+                NarratorMovementFlipFlop = true;
+                
+            }
             dialogueText.text += letter;
             yield return new WaitForSeconds(1f / lettersPerSecond);
         }
