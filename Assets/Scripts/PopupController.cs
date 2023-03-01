facd1644ab8ddf6039ea6705a8e75161d3818a7c
adamFentonAbertay <2001546@uad.ac.uk>
2023-03-01 15:38:24 +0000

    the popuups babyyy adblock!

--- Assets/Scripts/PopupController.cs
+++ Assets/Scripts/PopupController.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupController : MonoBehaviour
 {
+
+    [SerializeField] private RawImage popupImage;
+    [SerializeField] private TMPro.TextMeshProUGUI popupText;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    public void popupShow(GameObject toDisplay)
+    {
+        Debug.Log(toDisplay.name);
+        popupText.text = toDisplay.name;
+        popupImage.texture = toDisplay.GetComponent<RawImage>().texture;        
+    }
+    
+  
 
     // Update is called once per frame
     void Update()
