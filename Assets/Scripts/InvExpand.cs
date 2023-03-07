6231a8b26da571d0b8bff918c82d1c49ad8c911e
adamFentonAbertay <2001546@uad.ac.uk>
2023-03-07 16:29:02 +0000

    GO GO GO

--- Assets/Scripts/InvExpand.cs
+++ Assets/Scripts/InvExpand.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class InvExpand : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] GameObject button;
+    [SerializeField] RectTransform source;
+
+       void expandClick()
     {
+       
         
     }
-}
+  }
