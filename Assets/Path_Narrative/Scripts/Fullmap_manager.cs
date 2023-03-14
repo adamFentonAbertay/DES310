5f06dbf0eb5752a375420ab0fbbd59e08ece8d38
adamFentonAbertay <2001546@uad.ac.uk>
2023-03-14 12:58:17 +0000

    merge with tim

---
+++ Assets/Path_Narrative/Scripts/Fullmap_manager.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+public class Fullmap_manager : MonoBehaviour
+{
+    Vector3 touchStart;
+    Vector3 dir;
+
+    public Button[] map_Button;
+    public Sprite[] map_Image;
+    private int[] map_id = new int[7];
+    public static bool[] map_vivwed = new bool[7];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map_id[0] = 2;
+        map_id[3] = 2;
+        map_id[6] = 2;
+
+        map_id[1] = 1;
+        map_id[2] = 1;
+        map_id[4] = 1;
+        map_id[5] = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < map_id.Length; i++)
+        {
+            if (map_vivwed[i])
+            {
+                map_Button[i].GetComponent<Image>().sprite = map_Image[0];
+            }
+            else
+            {
+                map_Button[i].GetComponent<Image>().sprite = map_Image[1];
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Debug.Log("touch!!");
+        }
+        if (Input.GetMouseButton(0))
+        {
+            dir = touchStart - Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Camera.main.transform.position += dir;
+        }
+        zoom(Input.GetAxis("Mouse ScrollWheel")*2);
+
+    }
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, 1.0f, 40.0f);
+    }
+
+    public void SwitchMap(int mapid)
+    {
+        if (!map_vivwed[mapid])
+        {
+            map_vivwed[mapid] = true;
+        }
+        LoadMap(map_id[mapid]);
+        SceneManager.LoadScene("mini_map");
+    }
+
+    public void LoadMap(int mapid )
+    {
+        switch (mapid)
+        {
+            case 1:
+
+                minimap_manager.map_type = 0;
+
+                int[] tempMap = {4,0,2,0,1,4,
+                                 4,0,1,0,0,4,
+                                 4,0,0,1,0,4,
+                                 4,0,2,1,0,4,
+                                 4,0,0,0,2,4,
+                                 4,0,1,0,2,4,
+                                 4,0,2,1,0,4,
+                                 4,0,0,0,0,4};
+
+                
+
+                for (int i = 0; i < minimap_manager.gridId.Length; i++)
+                {
+                    minimap_manager.gridId[i] = tempMap[i];
+                }
+                break;
+            case 2:
+
+                minimap_manager.map_type = 1;
+
+                int[] tempMap2 = {4,0,2,0,1,4,4,4,
+                                  4,0,0,4,4,0,0,4,
+                                  4,4,4,0,2,1,0,4,
+                                  4,0,0,0,2,4,4,4,
+                                  4,0,2,4,4,0,2,4,
+                                  4,4,4,0,0,0,0,4};
+
+
+
+                for (int i = 0; i < minimap_manager.gridId.Length; i++)
+                {
+                    minimap_manager.gridId[i] = tempMap2[i];
+                }
+
+                break;
+        }
+    }
+}
---
+++ Assets/Path_Narrative/Scripts/monster_button.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+public class monster_button : MonoBehaviour
+{
+
+    float speed = 1.0f; //how fast it shakes
+    float amount = 1.0f; //how much it shakes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       gameObject.transform.position.Set(Mathf.Sin(Time.time * speed) * amount, Mathf.Sin(Time.time * speed) * amount, Mathf.Sin(Time.time * speed) * amount);
+       // gameObject.transform.position.Set(gameObject.transform.position.x+1, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+   public void monster_click()
+    {
+        Debug.Log("battle !!!!!");
+
+    }
+}
