b49109e6dfba8c2ed6d1ac5000135e60ebb0756a
adamFentonAbertay <2001546@uad.ac.uk>
2023-05-03 15:33:45 +0100

    empty

--- Assets/Scripts/PerPopupDialogue.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PerPopupDialogue : MonoBehaviour
-{
-    public List<string> myMessages;
-    public List<AudioClip> myAudio;
-}
